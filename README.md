963936033f3374af8f2ea8de576a895c6f2b6ebf
Roberts Peirags <rpeirags@gmail.com>
2020-09-21 22:28:25 +0200

    unstage

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Game Show App
-
+### [STATE] UNFINISHED 
+State is unfinished, still working on this.
 A word guessing game where players will click letters from an onscreen keyboard to try to guess a random phrase.
 
 ### [link to live view](https://indaqoo.github.io/techdegree-unit-6/)
--- public/app.js
+++ public/app.js
@@ -3,7 +3,6 @@ const phrase = document.querySelector("#phrase");
 const scoreBoard = document.querySelector('#scoreboard');
 let missed = 0;
 
-// phrases
 const phrases = [
   "hello world",
   "wheel of success",
@@ -11,21 +10,17 @@ const phrases = [
   "facebook",
   "strafe gaming",
 ];
-
-// event listener - start game
 const btn = document.querySelector(".btn__reset");
 btn.addEventListener("click", (e) => {
   const overlay = document.querySelector("#overlay");
   overlay.style.display = "none";
 });
 
-// This function gets random phrase from array
 function getRandomPhraseAsArray(array) {
   const randomPhrase = array[Math.floor(Math.random() * array.length)];
   return randomPhrase.split("");
 }
 
-// function that add  letters as a list item and adds a class letter or space
 function addPhraseToDisplay(arr) {
   for (let i = 0; i < arr.length; i++) {
     const li = document.createElement("li");
@@ -42,18 +37,6 @@ function addPhraseToDisplay(arr) {
 const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
 
-function checkLetter(button) {
-  const checkLetter = document.querySelectorAll('.letter');
-  let match = null;
-  for( let i = 0 ; i < checkLetter.length ; i += 1) {
-      if ( button.textContent === checkLetter[i].textContent ) {
-        checkLetter[i].classList.add('show');
-        match = button.textContent;
-      }
-  }
-  return match;
-}
-
 qwerty.addEventListener('click', (event) => {
   const button = event.target;
   if ( button.tagName === "BUTTON" ) {
@@ -66,8 +49,41 @@ qwerty.addEventListener('click', (event) => {
       hearts[missed].src = "images/lostHeart.png";
       hearts.length--;
       missed += 1;
-    };
+    }
+  checkWin();
   }
 
 
+  function checkLetter(button) {
+    const checkLetter = document.querySelectorAll('.letter');
+    let match = null;
+    for( let i = 0 ; i < checkLetter.length ; i += 1) {
+        if ( button.textContent === checkLetter[i].textContent ) {
+          checkLetter[i].classList.add('show');
+          match = button.textContent;
+        }
+    }
+    return match;
+  }
+
 });
+
+function checkWin() {
+  const letter = document.querySelectorAll('.letter');
+  const showLetter = document.querySelectorAll('.show');
+  const h2 = overlay.querySelector('h2');
+  const a = overlay.querySelector('a');
+  if ( letter.length === showLetter.length){
+    const overlay = document.querySelector("#overlay");
+    overlay.className = 'win';
+    overlay.style.display = "flex";
+    h2.textContent = "you won!";
+    a.textContent = "Try again?";
+  } else if ( missed > 4 ) {
+    const overlay = document.querySelector("#overlay");
+    overlay.className = 'lose';
+    overlay.style.display = "flex";
+    h2.textContent = "you lost :(";
+    a.textContent = "Try again?";
+  }
+};
