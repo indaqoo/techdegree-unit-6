2cd3c0cb0fe962d753036ae3949a729fa405e4ce
Roberts Peirags <rpeirags@gmail.com>
2020-09-21 11:30:48 +0200

    Updates

---
+++ app.js
@@ -0,0 +1,66 @@
+const qwerty = document.querySelector("#qwerty");
+const phrase = document.querySelector("#phrase");
+let missed = 0;
+
+// phrases
+const phrases = [
+  "hello world",
+  "wheel of success",
+  "social network",
+  "facebook",
+  "strafe gaming",
+];
+
+// event listener - start game
+const btn = document.querySelector(".btn__reset");
+btn.addEventListener("click", (e) => {
+  const overlay = document.querySelector("#overlay");
+  overlay.style.display = "none";
+});
+
+// This function gets random phrase from array
+function getRandomPhraseAsArray(array) {
+  const randomPhrase = array[Math.floor(Math.random() * array.length)];
+  return randomPhrase.split("");
+}
+
+// function that add  letters as a list item and adds a class letter or space
+function addPhraseToDisplay(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const li = document.createElement("li");
+    phrase.appendChild(li);
+    li.textContent = arr[i];
+    if (arr[i] !== " ") {
+      li.className = "letter";
+    } else {
+      li.className = "space";
+    }
+  }
+}
+
+const phraseArray = getRandomPhraseAsArray(phrases);
+addPhraseToDisplay(phraseArray);
+
+function checkLetter(button) {
+  const checkLetter = document.querySelectorAll('.letter');
+  let match = null;
+  for( let i = 0 ; i < checkLetter.length ; i += 1) {
+      if ( button.textContent === checkLetter[i].textContent ) {
+        checkLetter[i].classList.add('show');
+        match = button.textContent;
+      }
+  }
+  return match;
+}
+console.log( document.querySelectorAll('.letter'));
+
+qwerty.addEventListener('click', (event) => {
+  const button = event.target;
+  if ( button.tagName === "BUTTON" ) {
+    button.classList.add('chosen');
+    if (checkLetter(button)) {
+      button.style.background = '#76CE82';
+    }
+    button.disabled = true;
+  }
+});
